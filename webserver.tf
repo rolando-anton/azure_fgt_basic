daa64bb54502f5d7a5da92b34981fd5e23dab506
Rolando Anton <rolando@MacBook-Pro.local>
2018-12-11 01:48:00 -0500

    cloud init take 3

--- webserver.tf
+++ webserver.tf
@@ -39,26 +39,15 @@ resource "azurerm_virtual_machine" "webserver" {
 
     os_profile_linux_config {
         disable_password_authentication = false
+custom_data = << CUSTOM_DATA
+packages:
+ nginx
+
+CUSTOM_DATA
+
     }
     network_interface_ids = ["${azurerm_network_interface.webserver-nic.id}"]
 
 }
 
 
-resource "azurerm_virtual_machine_extension" "web-bootstrap" {
-  name                 = "webserver-script"
-  location             = "${var.location}"
-  resource_group_name  = "${azurerm_resource_group.resourcegroup.name}"
-  virtual_machine_name = "${azurerm_virtual_machine.webserver.name}"
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-    
-  settings = <<SETTINGS
-    {
-      "commandToExecute": "sudo apt -y update > /dev/null && sudo apt -y upgrade >> && sudo apt -y install nginx > /dev/null    && sudo systemctl start nginx > /dev/null"
-    }
-SETTINGS
-
-}
